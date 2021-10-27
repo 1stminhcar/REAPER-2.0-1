1a67b37825fabd357bd12b67c8c61a34b82d7068
NotMinhDucGamingTV <77107580+1stminhcar@users.noreply.github.com>
2021-10-27 18:11:53 +0700

    nuke da channel
    just nuke da channel

---
+++ Commands/Mod/nuke.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "nuke",
+    description: "nuke",
+     botPerms: ["ADMINISTRATOR"],
+    userPerms: ["ADMINISTRATOR"],
+             run: async(client, message, args) => {
+      message.channel.send("Nuking This channel....");
+			const position = message.channel.position;
+			const newChannel = await message.channel.clone();
+			await message.channel.delete();
+			newChannel.setPosition(position);
+      newChannel.send("Channel Nuked");
+			return newChannel.send("https://media1.tenor.com/images/e275783c9a40b4551481a75a542cdc79/tenor.gif?itemid=3429833")
+   }
+}
